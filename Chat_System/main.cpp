d2537e3147819b8d238fdc9c62690133710ac14e
Pawn0 <Pawn0@users.noreply.github.com>
2018-05-16 10:54:15 -0400

    create project with bare bones systems and documentation

---
+++ Chat_System/main.cpp
@@ -0,0 +1,5 @@
+#include <mysql/mysql.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{  }
---
+++ build/CMakeFiles/chatsystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatsystem.dir/Chat_System/main.cpp.o"
+  "chatsystem.pdb"
+  "chatsystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chatsystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
